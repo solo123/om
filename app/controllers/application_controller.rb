cf485df1cea428b4868f7f5fbfe3aeab260a6114
Jimmy Liang <lyh@yazotech.com>
2013-04-08 21:10:13 +0800

    v1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      if resource_name == :employee
+        "travel_admin/login"
+      else
+        "travel_website/login"
+      end
+    else
+      "travel_website/application"
+    end
+  end
 end
