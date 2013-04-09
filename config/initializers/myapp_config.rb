ad40bbfe92d16f701fea2980b8be0acf5b7f3f04
Jimmy Liang <lyh@yazotech.com>
2013-04-09 22:09:16 +0800

    v1.01

--- config/initializers/myapp_config.rb
+++ config/initializers/myapp_config.rb
@@ -1,2 +1,3 @@
 require "#{Rails.root}/lib/core/all_changes.rb"
-
+require 'i18n/missing_translations'
+Omei::Application.config.app_middleware.use(I18n::MissingTranslations) if Rails.env.development?
