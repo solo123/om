62a0a62ffd8ea42fcde9aadcfe447153cf826a84
Jimmy Liang <lyh@yazotech.com>
2013-04-16 01:16:22 +0800

    change admin into Bootstrap

--- config/initializers/myapp_config.rb
+++ config/initializers/myapp_config.rb
@@ -1,3 +1,33 @@
 require "#{Rails.root}/lib/core/all_changes.rb"
 #require 'i18n/missing_translations'
 #Omei::Application.config.app_middleware.use(I18n::MissingTranslations) if Rails.env.development?
+#
+
+module WillPaginate
+  module ActionView
+    def will_paginate(collection = nil, options = {})
+      options[:renderer] ||= BootstrapLinkRenderer
+      super.try :html_safe
+    end
+
+    class BootstrapLinkRenderer < LinkRenderer
+      protected
+      
+      def html_container(html)
+        tag :div, tag(:ul, html), container_attributes
+      end
+
+      def page_number(page)
+        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+      end
+
+      def previous_or_next_page(page, text, classname)
+        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+      end
+    end
+  end
+end
+
+WillPaginate::ViewHelpers.pagination_options[:class] = 'pagination pagination-mini'
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&laquo;'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '&raquo;'
